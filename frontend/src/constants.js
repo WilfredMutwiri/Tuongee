b10fef9681651344a9f6bcb04f03236ab8f73ea1
Wilfred Mutwiri <Wilfred Mutwiri>
2024-03-01 13:08:19 +0300

    change server url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,2 @@
-export const SERVER_URL="http://localhost:4000"
-// export const SERVER_URL="https://tuongee-syi9.vercel.app"
+// export const SERVER_URL="http://localhost:4000"
+export const SERVER_URL="https://tuongee.onrender.com"
